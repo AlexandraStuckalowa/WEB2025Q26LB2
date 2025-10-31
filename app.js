5fc4796a83d7b44fc07f23c418ba303ad8720ab5
Alex <aleksansra.stuckalowa@yandex.ru>
2025-10-31 17:29:42 +0300

    добавлен заголовок

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ app.className = 'app';
 
 // Добавляем ее на страницу
 document.body.appendChild(app);
+
+// Создаём заголовок
+const title = document.createElement('h1');
+title.textContent = 'Мой список дел';
+app.appendChild(title);
