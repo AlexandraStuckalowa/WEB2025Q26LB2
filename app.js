e0268f9643b7f84002dbc97d532dc7fb3c8ea5ce
Alex <aleksansra.stuckalowa@yandex.ru>
2025-10-31 17:43:47 +0300

    добавлена форма для добавления задач

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ document.body.appendChild(app);
 const title = document.createElement('h1');
 title.textContent = 'Мой список дел';
 app.appendChild(title);
+
+// Создаём форму для добавления задач
+const form = document.createElement('form');
+form.className = 'task-form';
+
+// Создаем поле для ввода текста задачи
+const inputText = document.createElement('input');
+inputText.type = 'text';
+inputText.placeholder = 'Введите задачу...';
+inputText.required = true;
